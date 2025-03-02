0ab3f10ff59bf8d9aa90ed82c2b566a8b9bdf865
Trịnh Vân <socxinh26@gmail.com>
2025-03-02 22:11:03 +0700

    Add files via upload

---
+++ GuessIt.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+using namespace std;
+int generateRandomNumber()
+{
+    return rand() % 100 + 1;
+}
+int getPlayerGuess()
+{
+    int guess;
+    cout << endl << "Enter your guess (1..100): ";
+    cin >> guess;
+    return guess;
+
+}
+void printAnswer(int guess, int secretNumber)
+{
+    if (guess > secretNumber) {
+cout << "Your number is too big." << endl;
+} else if (guess < secretNumber) {
+cout << "Your number is too small." << endl;
+} else {
+cout << "Congratulation! You win." << endl;
+}
+}
+int main()
+{
+    srand(time(0));
+{
+    int secretNumber = generateRandomNumber();
+    int guess;
+    do {
+            guess = getPlayerGuess();
+            printAnswer(guess, secretNumber);
+    }
+    while (guess != secretNumber);
+}
+return 0;
+}
